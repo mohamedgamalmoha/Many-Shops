9817490d5479bd1b4d71b5015cf1fba92f2972f6
= <=>
2024-11-01 20:39:27 +0200

    Customize superuser admin

--- restaurant/admin/base.py
+++ restaurant/admin/base.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 
 
 class DeactivateLogEntryMixin:
@@ -52,3 +53,36 @@ class PermissionsAllowOwnerAdminMixin:
 
     def has_change_permission(self, request, obj=None):
         return obj is None or obj.is_owner(request.user)
+
+
+class PermissionsReadOnlyAdminMixin:
+
+    def has_view_or_change_permission(self, request, obj=None):
+        return True
+
+    def has_module_permission(self, request):
+        return True
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        return None
+
+    def save_model(self, request, obj, form, change):
+        raise PermissionDenied
+
+    def delete_model(self, request, obj):
+        raise PermissionDenied
+
+    def save_related(self, request, form, formsets, change):
+        raise PermissionDenied
--- restaurant/admin/superuser.py
+++ restaurant/admin/superuser.py
@@ -1,25 +1,90 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from ..models import HeaderImage, SocialMediaLink, ProductVariant
+from .base import PermissionsReadOnlyAdminMixin
 
 
-class RestaurantSuperuserAdmin(admin.ModelAdmin):
-    ...
+class BaseVariantInlineAdmin(admin.StackedInline):
+    extra = 1
+    min_num = 1
+    max_num = 5
+    can_delete = True
+    show_change_link = True
+    readonly_fields = ('create_at', 'update_at')
 
 
-class HeaderImageSuperuserAdmin(admin.ModelAdmin):
-    ...
+class ProductVariantInlineAdmin(BaseVariantInlineAdmin):
+    model = ProductVariant
 
 
-class SocialMediaLinkSuperuserAdmin(admin.ModelAdmin):
-    ...
+class SocialMediaLinkInlineAdmin(BaseVariantInlineAdmin):
+    model = SocialMediaLink
 
 
-class CategorySuperuserAdmin(admin.ModelAdmin):
-    ...
+class HeaderImageInlineAdmin(BaseVariantInlineAdmin):
+    model = HeaderImage
 
 
-class ProductSuperuserAdmin(admin.ModelAdmin):
-    ...
+class RestaurantSuperuserAdmin(PermissionsReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ['name', 'owner', 'is_active', 'create_at', 'update_at']
+    readonly_fields = ['create_at', 'update_at']
+    list_filter = ['is_active']
+    fieldsets = (
+        (_('Main Info'), {'fields': ('owner', 'name', 'description', 'email', 'contact_number')}),
+        (_('Address'), {'fields': ('city', 'state', 'zip_code')}),
+        (_('More Info'), {'fields': ('opening_time', 'closing_time', 'is_active')}),
+        (_('Important Dates'), {'fields': ('create_at', 'update_at')}),
+    )
+    inlines = [HeaderImageInlineAdmin, SocialMediaLinkInlineAdmin]
 
 
-class ProductVariantSuperuserAdmin(admin.ModelAdmin):
-    ...
+class HeaderImageSuperuserAdmin(PermissionsReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ['restaurant', 'is_active', 'create_at', 'update_at']
+    list_filter = ['is_active']
+    readonly_fields = ['create_at', 'update_at']
+    fieldsets = (
+        (_('Main Info'), {'fields': ('alt', 'image', 'url', 'is_active')}),
+        (_('Important Dates'), {'fields': ('create_at', 'update_at')}),
+    )
+
+
+class SocialMediaLinkSuperuserAdmin(PermissionsReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ['restaurant', 'is_active', 'create_at', 'update_at']
+    list_filter = ['is_active']
+    readonly_fields = ['create_at', 'update_at']
+    fieldsets = (
+        (_('Main Info'), {'fields': ('restaurant', 'platform', 'url', 'is_active')}),
+        (_('Important Dates'), {'fields': ('create_at', 'update_at')}),
+    )
+
+
+class CategorySuperuserAdmin(PermissionsReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ['name', 'restaurant', 'is_active', 'create_at', 'update_at']
+    list_filter = ['is_active']
+    readonly_fields = ['create_at', 'update_at']
+    fieldsets = (
+        (_('Main Info'), {'fields': ('restaurant', 'name', 'is_active')}),
+        (_('Important Dates'), {'fields': ('create_at', 'update_at')}),
+    )
+
+
+class ProductSuperuserAdmin(PermissionsReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ['name', 'category', 'is_active', 'create_at', 'update_at']
+    list_filter = ['is_active']
+    readonly_fields = ['create_at', 'update_at']
+    fieldsets = (
+        (_('Main Info'), {'fields': ('category', 'name', 'description')}),
+        (_('More Info'), {'fields': ('price', 'image', 'is_active')}),
+        (_('Important Dates'), {'fields': ('create_at', 'update_at')}),
+    )
+    inlines = [ProductVariantInlineAdmin]
+
+
+class ProductVariantSuperuserAdmin(PermissionsReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ['name', 'product', 'create_at', 'update_at']
+    readonly_fields = ['create_at', 'update_at']
+    fieldsets = (
+        (_('Main Info'), {'fields': ('product', 'name', 'price', 'is_active')}),
+        (_('Important Dates'), {'fields': ('create_at', 'update_at')}),
+    )
