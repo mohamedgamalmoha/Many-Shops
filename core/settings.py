f4ddd3af164bbe46e714b163193724c8c30ed477
= <=>
2024-12-11 20:35:21 +0200

    Translate proj jazzmin title

--- core/settings.py
+++ core/settings.py
@@ -421,13 +421,13 @@ SPECTACULAR_SETTINGS = {
 #: and other UI-related settings for Django Admin.
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "Menus",
+    "site_title": _("Menus"),
 
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "Menus",
+    "site_header": _("Menus"),
 
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "Menus",
+    "site_brand": _("Menus"),
 
     # Logo to use for your site, must be present in static files, used for brand on top left
     "site_logo": "images/logo.png",
@@ -476,8 +476,7 @@ JAZZMIN_SETTINGS = {
     #############
 
     # Additional links to include in the user menu on the top right ("app" url type is not allowed)
-    "usermenu_links": [
-    ],
+    "usermenu_links": [],
 
     #############
     # Side Menu #
@@ -499,8 +498,7 @@ JAZZMIN_SETTINGS = {
     "order_with_respect_to": [],
 
     # Custom links to append to app groups, keyed on app name
-    "custom_links": {
-    },
+    "custom_links": {},
 
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
     # for the full list of 5.13.0 free icon classes
