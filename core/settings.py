5c667f18c3c9cd01b11bc0acec4015b51c1e8c5f
= <=>
2024-12-15 13:30:16 +0200

    Remove unused configs

--- core/settings.py
+++ core/settings.py
@@ -454,7 +454,7 @@ JAZZMIN_SETTINGS = {
 
     # List of model admins to search from the search bar, search bar omitted if excluded
     # If you want to use a single search field you dont need to use a list, you can use a simple string
-    "search_model": ["auth.User", 'restaurant.Restaurant'],
+    "search_model": [],
 
     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
     "user_avatar": None,
@@ -468,7 +468,7 @@ JAZZMIN_SETTINGS = {
 
         # Url that gets reversed (Permissions can be added)
         {"name": _("Home"),  "url": "admin:index", "permissions": ["auth.view_user"]},
-        {"name": _("API Docs"), "url": 'swagger_docs_view'},
+        # {"name": _("API Docs"), "url": 'swagger_docs_view'},
         # model admin to link to (Permissions checked against model)
         {},
     ],
@@ -505,8 +505,7 @@ JAZZMIN_SETTINGS = {
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
     # for the full list of 5.13.0 free icon classes
     "icons": {
-        # "auth": "fas fa-users-cog",
-        "auth.User": "fa-solid fa-users",
+        "accounts.User": "fa-solid fa-users",
         "restaurant.Restaurant": "fa-solid fa-utensils",
         "restaurant.HeaderImage": "fa-regular fa-image",
         "restaurant.SocialMediaLink": "fa-solid fa-link",
