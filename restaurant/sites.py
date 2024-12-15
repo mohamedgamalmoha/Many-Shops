614d0d73380c3be7406ba1995bf92500d5e4e81c
= <=>
2024-12-15 13:31:41 +0200

    Custom get app list function for sites

--- restaurant/sites.py
+++ restaurant/sites.py
@@ -1,6 +1,46 @@
+from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.contrib.admin.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _
+
+
+APP_MODELS_ORDER = {
+    'restaurant': ['Restaurant', 'Category', 'Product', 'ProductVariant', 'ProductType'],
+    'info': ['MainInfo', 'AboutUs', 'Theme', 'Service', 'ContactUs']
+}
+
+
+def get_app_list(self, request, app_label=None):
+    """
+    Override get_app_list to sort models based on model_order and handle missing apps/models.
+    """
+    app_dict = self._build_app_dict(request)
+
+    # Sort the apps alphabetically
+    app_list = sorted(app_dict.values(), key=lambda x: x["name"].lower())
+
+    for app in app_list:
+        app_label = app['app_label']
+
+        # Check if the app is in the APP_MODELS_ORDER
+        if app_label in APP_MODELS_ORDER:
+            defined_order = APP_MODELS_ORDER[app_label]
+            # Sort models based on the defined order, adding undefined models at the end
+            app['models'].sort(
+                key=lambda x: (
+                    defined_order.index(x['object_name'])
+                    if x['object_name'] in defined_order
+                    else len(defined_order)  # Undefined models go to the end
+                )
+            )
+        else:
+            # For apps not in model_order, keep the default order
+            app['models'].sort(key=lambda x: x['object_name'])  # Alphabetical order
+
+    return app_list
+
+
+admin.AdminSite.get_app_list = get_app_list
 
 
 class CustomerAdminSite(AdminSite):
